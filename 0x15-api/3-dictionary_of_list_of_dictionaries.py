5293458b85e9dc3584fd5378b6d7686e9790dc3d
victorpreston <prestonvictor25@gmail.com>
2024-01-31 09:52:43 -0800

    0x15-api

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Exports to-do list information of all employees to JSON format."""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "username": u.get("username"),
+                "task": t.get("title"),
+                "completed": t.get("completed")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
