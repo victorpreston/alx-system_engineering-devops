d576d8102aca60ed8935dbd99b3a1c598a80029f
victorpreston <prestonvictor25@gmail.com>
2024-03-22 15:15:14 -0700

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install an especific version of flask
+
+exec { 'uninstall_flask':
+  command => 'pip3 uninstall -y flask',
+  path    => ['/usr/bin', '/usr/sbin'],
+  before  => Package['flask'],
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
