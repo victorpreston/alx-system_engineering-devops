c33bda3cb9fda76d06a8d64dae7629e497434f8b
victorpreston <prestonvictor25@gmail.com>
2024-03-22 15:21:00 -0700

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask
-
-exec { 'uninstall_flask':
-  command => 'pip3 uninstall -y flask',
-  path    => ['/usr/bin', '/usr/sbin'],
-  before  => Package['flask'],
-}
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
