947e4148302c846746235f1201cff5b4aaedbdca
victorpreston <prestonvictor25@gmail.com>
2024-03-22 16:32:43 -0700

    hjhjh

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,11 @@
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => 'pip3 install flask-2.1.0',
+  path => ['/usr/bin'],
+  unless => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
 
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
+exec { 'install_werkzeug':
+  command => 'pip3 install werkzeug-2.1.1',
+  path => ['/usr/bin'],
+  unless => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
