b7c403a9da5c3cc4158402954bdfc8289472510f
victorpreston <prestonvictor25@gmail.com>
2024-03-22 16:57:42 -0700

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,13 @@
-# Install Flask using Puppet
+# install flask using Puppet
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['usr/bin'],
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
 
 exec { 'install_werkzeug':
-  command => '/usr/bin/pip3 install werkzeug==2.1.',
-  unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1."',
+    command => '/usr/bin/pip3 install werkzeug==2.1.1',
+    path    => ['usr/bin'],
+    unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
