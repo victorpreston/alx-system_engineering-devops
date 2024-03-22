818c7006f02b0c88be66ffe621a6e9d1c6af052f
victorpreston <prestonvictor25@gmail.com>
2024-03-22 14:38:36 -0700

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#install puppet-lint
-package{'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem'
+# Install flask package
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
