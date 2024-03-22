f7fab19691fd11d0fc6e11563bc377b431014e9d
victorpreston <prestonvictor25@gmail.com>
2024-03-22 16:01:43 -0700

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,27 +1,9 @@
-# Install Flask 2.1.0
-exec { 'uninstall_flask':
-  command => 'pip3 uninstall -y flask',
-  path    => ['/usr/bin', '/usr/sbin'],
-  onlyif  => 'pip3 show flask',
-  before  => Package['flask_2.1.0'],
-}
-
-package { 'flask_2.1.0':
+package { 'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
   provider => 'pip3',
 }
 
-# Install Flask 2.1.1
-exec { 'uninstall_flask_2.1.0':
-  command => 'pip3 uninstall -y flask',
-  path    => ['/usr/bin', '/usr/sbin'],
-  onlyif  => 'pip3 show flask',
-  before  => Package['flask_2.1.1'],
-}
-
-package { 'flask_2.1.1':
+package { 'Werkzeug':
   ensure   => '2.1.1',
-  name     => 'flask',
   provider => 'pip3',
 }
