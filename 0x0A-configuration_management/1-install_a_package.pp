1b65e05704e33ab65ab1c80b28b95cae74b651de
victorpreston <prestonvictor25@gmail.com>
2024-03-22 17:03:39 -0700

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,12 +2,12 @@
 
 exec { 'install_flask':
     command => '/usr/bin/pip3 install flask==2.1.0',
-    path    => ['usr/bin'],
+    path    => ['/usr/bin', '/bin'],
     unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
 
 exec { 'install_werkzeug':
     command => '/usr/bin/pip3 install werkzeug==2.1.1',
-    path    => ['usr/bin'],
+    path    => ['/usr/bin', '/bin'],
     unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
